39ee12de426de4ed1a8b7ae059326de662a60d07
Yunseon Shin <ysshin@postech.ac.kr>
2023-11-30 19:32:50 +0900

    llvm custom cse

--- extension_backends/llvm_codegen_backend.py
+++ extension_backends/llvm_codegen_backend.py
@@ -3,11 +3,14 @@ import contextlib
 from typing import List
 from typing import Set
 from typing import Dict
-from torch._inductor.codegen import cpp, wrapper, common
+from torch._inductor.codegen import cpp, wrapper
+from . import common
 from torch._inductor.scheduler import BaseScheduling
 from torch._inductor.virtualized import V
 from torch._inductor.utils import IndentedBuffer
 import sympy
+
+cexpr = cpp.CppPrinter().doprint
 
 class ExtensionWrapperCodegen(wrapper.WrapperCodeGen):
     def __init__(self):
@@ -18,9 +21,8 @@ class ExtensionOverrides(common.OpOverrides):
 
 class ExtensionKernel(common.Kernel):
     overrides = ExtensionOverrides
-    newvar_prefix = ""
-    suffix = ""
-
+    newvar_prefix = "auto "
+    suffix = ';'
     def __init__(self, args=None):
         super().__init__(args)
         self.call_ranges = None
@@ -33,25 +35,113 @@ class ExtensionKernel(common.Kernel):
         self.reduction_cse = common.CSE(self.newvar_prefix, self.suffix, name_prefix="tmp_acc")
 
     def load(self, name: str, index: sympy.Expr):
-        pass
+        index = self.rename_indexing(index)
+        var = self.args.input(name)
+        ssa_num = var[-1]
+        line = f"{var}[{index}]"
+        dtype = V.graph.get_dtype(name)
+        type_name = cpp.DTYPE_TO_CPP[dtype]
+        self.reduction_prefix.writeline(f'%arrayidx{ssa_num} = getelementptr inbounds {type_name}, ptr %{var}, i64 {index}') # TODO: index
+        self.reduction_prefix.writeline(f'%{ssa_num} = load {type_name}, ptr %arrayidx{ssa_num}, align 4') # TODO: align
+        self.cse.prefix = type_name + " "
+        return self.cse.generate(self.loads, line)
 
     def store(self, name: str, index: sympy.Expr, value, *args, **kwargs):
-        pass
+        index = self.rename_indexing(index)
+        var = self.args.output(name)
+        line = f"{var}[{index}] = {value}"
+        self.reduction_prefix.writeline(f"%arrayidx = getelementptr inbounds {cpp.DTYPE_TO_CPP[V.graph.get_dtype(name)]}, ptr %{var}, i64 {index}")
+        self.reduction_prefix.writeline(f"store {cpp.DTYPE_TO_CPP[V.graph.get_dtype(name)]} {value}, ptr %arrayidx, align 4") # TODO: ``index number, value
+        self.cse.generate(self.stores, line, assignment = False)
 
     def reduction(self, dtype, src_dtype, reduction_type, value):
-        pass
+        argmax_or_argmin = reduction_type in {"argmax", "argmin"}
+        if argmax_or_argmin:
+            raise NotImplementedError() #TODO: argmin, argmax
+        else:
+            reduction_key = src_dtype, reduction_type, value
+            acc = self.reduction_cse.generate(
+                self.loads, f"reduction {reduction_key}", write=False
+            )
+            self.reduction_vars[acc] = reduction_type
+            acc_type = cpp.reduction_acc_type(reduction_type, dtype)
+            self.reduction_prefix.writeline(f"{acc_type} {acc} = {cpp.reduction_init(reduction_type, dtype)};")
+            line = f"{acc} = {cpp.reduction_combine(reduction_type, acc, value)}"
+            self.cse.generate(self.stores, line, assignment = False)
+            self.reduction_cse.reduction_cache[reduction_key] = acc
+        return acc
 
     def store_reduction(self, name, index, value):
-        pass
+        index = self.rename_indexing(index)
+        var = self.args.output(name)
+        self.reduction_suffix.writeline(f"{var}[{index}] = {value};")
 
     def codegen_loops(self):
-        pass
+        code = common.BracesBuffer()
+        # Loop body part
+        loops = [LoopLevel(var, size) for var, size in zip(self.itervars, self.ranges)]
+        loops, reductions = [LoopNest(loops[: self.reduction_depth]),
+                             LoopNest(loops[self.reduction_depth :])]
+        reductions.mark_reduction(self.reduction_vars)
+
+        with contextlib.ExitStack() as stack:
+            loops.codegen(code, stack)
+            with contextlib.ExitStack() as stack_outer:
+                if self.reduction_prefix:
+                    stack_outer.enter_context(code.indent())
+                code.splice(self.reduction_prefix)
+                with contextlib.ExitStack() as stack:
+                    reductions.codegen(code, stack)
+                    code.splice(self.loads)
+                    code.splice(self.compute)
+                    code.splice(self.stores)
+                code.splice(self.reduction_suffix)
+        return code
 
     def codegen_kernel(self, wrapper):
-        pass
+        arg_defs, call_args, arg_types = self.args.cpp_argdefs()
+        arg_defs = ",\n".ljust(25).join(arg_defs)
+        arg_types = ",".join(arg_types)
+        code = common.BracesBuffer()
+
+        # Todo. kernel name custom
+        kernel_name = f"Extensin_Kernel"
+        kernel_decl_name = kernel_name if V.graph.cpp_wrapper else "kernel"
+        code.writeline(f'extern "C" void {kernel_decl_name}({arg_defs})')
+        with code.indent():
+            for old, new in self.args.aliases():
+                code.writeline(f"auto {old} = {new};")
+            # Loop body part
+            code.splice(self.codegen_loops())
+
+        codecache_def = IndentedBuffer()
+        if not V.graph.cpp_wrapper:
+            codecache_def.writeline("async_compile.cpp('''")
+        codecache_def.splice(code)
+        if not V.graph.cpp_wrapper:
+            codecache_def.writeline("''')")
+
+        wrapper.define_kernel(kernel_name, codecache_def.getvalue(), cuda=False)
+        # generate the code to call this
+        wrapper.generate_kernel_call(kernel_name, call_args, cuda=False)
+        print(code.getvalue())
+        return code.getvalue()
 
     def set_ranges(self, lengths, reduction_lengths):
-        pass
+        if self.call_ranges:
+            assert self.call_ranges == tuple(lengths) + tuple(
+                reduction_lengths
+            ), f"{self.call_ranges} == {tuple(lengths)} + {tuple(reduction_lengths)}"
+            assert self.reduction_depth == len(lengths)
+        else:
+            self.call_ranges = tuple(lengths) + tuple(reduction_lengths)
+            self.ranges = [self.rename_indexing(x) for x in self.call_ranges]
+            self.itervars = [sympy.Symbol(f"i{n}") for n in range(len(self.ranges))]
+            self.reduction_depth = len(lengths)
+        return (
+            self.itervars[: self.reduction_depth],
+            self.itervars[self.reduction_depth :],
+        )
 
 @dataclasses.dataclass
 class LoopLevel:
@@ -60,8 +150,10 @@ class LoopLevel:
     reduction_vars: Dict[str, str] = None
 
     # Todo. Type change for reduction
+    INDEX_TYPE = "long"
     def lines(self):
-        pass
+        line = f"for({self.INDEX_TYPE} {self.var}=0; {self.var}<{cexpr(self.size)}; ++{self.var})"
+        return [line]
 
 @dataclasses.dataclass
 class LoopNest:
@@ -105,11 +197,10 @@ class ExtensionScheduling(BaseScheduling):
         _, (group, reduction_group) = max(
             nodes, key=lambda x: int(x.is_reduction())
         ).group
-
-        for node in nodes:
-            ex_kernel = ExtensionKernel()
-            vars, reduction_vars = ex_kernel.set_ranges(group, reduction_group)
-            with ex_kernel:
+        ex_kernel = ExtensionKernel()
+        with ex_kernel as kernel:
+            for node in nodes:
+                vars, reduction_vars = ex_kernel.set_ranges(group, reduction_group)
                 node.run(vars, reduction_vars)
 
         wrapper = V.graph.wrapper_code
