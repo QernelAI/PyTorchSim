b6e1020e6de9ab15ef868f44bd86b72b91189889
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2025-06-13 04:18:24 +0000

    [Tests] Add Matmul reduction fusion case

---
+++ tests/Fusion/test_matmul_reduction.py
@@ -0,0 +1,46 @@
+import torch
+import torch._dynamo
+import torch.utils.cpp_extension
+
+def test_result(name, out, cpu_out, rtol=1e-4, atol=1e-4):
+    if torch.allclose(out.cpu(), cpu_out, rtol=rtol, atol=atol):
+        message = f"|{name} Test Passed|"
+        print("-" * len(message))
+        print(message)
+        print("-" * len(message))
+    else:
+        message = f"|{name} Test Failed|"
+        print("-" * len(message))
+        print(message)
+        print("-" * len(message))
+        print("custom out: ", out.cpu())
+        print("cpu out: ", cpu_out)
+        exit(1)
+
+def test_matmul_reduce(device):
+    def matmul_fused(a, b, c):
+        result = torch.matmul(a, b)
+        return result, result.max(dim=-1).values
+    torch.manual_seed(0)
+    input = torch.randn(512, 128)
+    weight = torch.randn(128, 512)
+    x1 = input.to(device=device)
+    w1 = weight.to(device=device)
+    x2 = input.to("cpu")
+    w2 = weight.to("cpu")
+    c = 7
+    opt_fn = torch.compile(dynamic=False)(matmul_fused)
+    res = opt_fn(x1, w1, c)
+    y = matmul_fused(x2, w2, c)
+    test_result("Matmul Scalar Fusion Forward", res[0], y[0])
+    test_result("Matmul Scalar Fusion Forward", res[1], y[1])
+
+if __name__ == "__main__":
+    import os
+    import sys
+    sys.path.append(os.environ.get('TORCHSIM_DIR', default='/workspace/PyTorchSim'))
+
+    from Scheduler.scheduler import ExecutionEngine
+    module = ExecutionEngine.setup_device()
+    device = module.custom_device()
+    test_matmul_reduce(device)
