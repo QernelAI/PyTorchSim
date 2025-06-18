97dc265ddaab48301ed89f400926daf91afef8e0
Yunseon Shin <yunseon0518@naver.com>
2025-06-18 04:34:37 +0000

    [fix] prevent infinite loop in auto-tune

--- PyTorchSimFrontend/mlir/mlir_codegen_backend.py
+++ PyTorchSimFrontend/mlir/mlir_codegen_backend.py
@@ -1388,12 +1388,16 @@ class MLIRKernel(mlir_common.BaseMLIRKernel):
         choices = []
         initial_tile_size = self.kernel_group.tile_desc.get_tile_size()
         previous_ranges = self.ranges
+        prevent_infinite_loop = 0
+        if len(initial_tile_size) < 2:
+            return choices # Can't autotune for 1-D tile size
         for vlane_stride in [2, 4, 8]:
             os.environ['TORCHSIM_VECTOR_LANE_STRIDE'] = str(vlane_stride)
             previous_tile_size = initial_tile_size
             increase_dim = 0 # increase the first dimension
-            while previous_tile_size[increase_dim] * 2 <= previous_ranges[increase_dim] and previous_tile_size[increase_dim] <= 2 ** 13:
+            while previous_tile_size[increase_dim] * 2 <= previous_ranges[increase_dim] and previous_tile_size[increase_dim] <= 2 ** 13 and prevent_infinite_loop < 10:
                 incrase_dim = -1 # only increase the last dimension
+                prevent_infinite_loop += 1
                 while previous_tile_size[incrase_dim] * 2 <= previous_ranges[incrase_dim] and previous_tile_size[incrase_dim] <= 2 ** 13:
                     src_code = super().codegen_nodes(nodes, kernel_name)
                     if self.stop_autotune:
